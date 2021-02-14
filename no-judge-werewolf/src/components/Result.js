b7a9ad70e06509da1a8f7e2a4489658b6272451d
Shang-Hung Tsai <shtsai7@gmail.com>
2021-02-13 22:07:20 -0800

    Add log section

--- no-judge-werewolf/src/components/Result.js
+++ no-judge-werewolf/src/components/Result.js
@@ -6,14 +6,20 @@ class Result extends React.Component {
         this.state = {
             name: 'Result',
             should_show_result: false,
+            should_show_log: false,
         }
 
         this.flip_show_result = this.flip_show_result.bind(this);
+        this.flip_show_log = this.flip_show_log.bind(this);
         this.get_dead = this.get_dead.bind(this);
     }
 
     flip_show_result() {
         this.setState({ should_show_result: !this.state.should_show_result });
+    }
+
+    flip_show_log() {
+        this.setState({ should_show_log: !this.state.should_show_log });
     }
 
     get_dead() {
@@ -29,11 +35,59 @@ class Result extends React.Component {
         if (this.props.current === this.state.name) {
             const dead = this.get_dead();
             var msg = dead.size === 0 ? "昨晚是平安夜" : "昨晚死的是：" + [...dead].join(',');
+
             return (
                 <div className="Result">
-                    <button onClick={this.flip_show_result}>顯示結果</button>
-                    {this.state.should_show_result && <p>{msg}</p>}
-                </div>
+                    <div className="Dead">
+                        <button onClick={this.flip_show_result}>顯示結果</button>
+                        {this.state.should_show_result && <p>{msg}</p>}
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <p>----------------- 法官Only -----------------</p>
+                    <div className="Log">
+                        <button onClick={this.flip_show_log}>請看VCR</button>
+                        {this.state.should_show_log &&
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            狼人殺了：
+                                        </td>
+                                        <td>
+                                            {this.props.killed_by_werewolf}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            長老是：
+                                        </td>
+                                        <td>
+                                            {this.props.ancient_index}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            守衛守了：
+                                        </td>
+                                        <td>
+                                            {this.props.saved_by_savior}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            女巫救了：
+                                        </td>
+                                        <td>
+                                            {this.props.saved_by_witch ? 'Yes' : 'No'}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        }
+                    </div>
+                </div >
             );
         } else {
             return <div></div>
